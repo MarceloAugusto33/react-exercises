1e2ed337eddd224a43fca3360b8d8caa6d2c68e2
marcelo <sjsjsjmarcelo@gmail.com>
2024-01-26 22:35:36 -0300

    ex06 completed

---
+++ ex06/README.md
@@ -0,0 +1,2 @@
+6 - Validação de Formulário:
+Faça um formulário de login simples com campos de nome de usuário e senha. Utilize o useState para armazenar os valores e o useEffect para validar a senha (por exemplo, deve conter pelo menos 8 caracteres).
---
+++ ex06/src/App.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+export function App() {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [userValid, setUserValid] = useState(false)
+  const [passwordValid, setPasswordValid] = useState(false)
+
+
+  useEffect(() =>{
+    if(password.length > 8){
+      setPasswordValid(true)
+    } else{
+      setPasswordValid(false)
+    }
+
+    if(user.length > 4){
+      setUserValid(true)
+    } else{
+      setUserValid(false)
+    }
+  }, [user, password])
+
+  function postForm(e) {
+    if(!userValid && !passwordValid){
+      e.preventDefault()
+      return
+    }
+    
+    console.log("Formulario enviado")
+  }
+
+
+  return (
+    <div className="container">
+      <div className="form__wrapper">
+        <h2>Login</h2>
+        <form>
+          <label htmlFor="user">Usuario:</label>
+
+          <input
+            type="text"
+            id='user'
+            placeholder='usuario'
+            className={userValid? 'valid': ''}
+            onChange={e => setUser(e.target.value)}
+          />
+
+          <label htmlFor="password">Senha:</label>
+
+          <input
+            type="password"
+            id='password'
+            placeholder='password'
+            className={passwordValid? 'valid': ''}
+            onChange={e => setPassword(e.target.value)}
+          />
+
+          <button
+            onClick={postForm}
+          >Entrar</button>
+        </form>
+      </div>
+
+    </div>
+
+  )
+}
+
